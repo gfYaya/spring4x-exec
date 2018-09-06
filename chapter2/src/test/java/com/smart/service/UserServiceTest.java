f91f2597df944d04010f0f19138c9b011262cdb9
gfYaya <2645919729@qq.com>
2018-09-07 00:12:22 +0800

    spring 项目chapter 2 练习

--- chapter2/src/test/java/com/smart/service/UserServiceTest.java
+++ chapter2/src/test/java/com/smart/service/UserServiceTest.java
@@ -1,9 +1,14 @@
 package com.smart.service;
 
+import com.smart.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.testng.annotations.Test;
+
+import java.util.Date;
+
+import static org.testng.Assert.*;
 
 @ContextConfiguration("classpath*:/smart-context.xml")
 public class UserServiceTest extends AbstractTransactionalTestNGSpringContextTests {
@@ -22,4 +27,23 @@ public class UserServiceTest extends AbstractTransactionalTestNGSpringContextTes
         assert b1==true;
         assert b2==false;
     }
+
+    @Test
+    public void findUserByUserName(){
+        for(int i =0; i< 100;i++) {
+            User user = userService.findUserByUserName("admin");
+            assertEquals(user.getUserName(),"admin");
+        }
+    }
+
+
+    public void testAddLoginLog(){
+        User user = userService.findUserByUserName("admin");
+        user.setUserId(1);
+        user.setUserName("admin");
+        user.setLastIp("192.168.12.7");
+        user.setLastVisit(new Date());
+        userService.loginSuccess(user);
+    }
+
 }
