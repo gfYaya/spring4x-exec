ae8357f4f37be22f0c02bcb16cd26b9c5448424a
gfYaya <2645919729@qq.com>
2018-09-08 23:19:24 +0800

    spring 项目chapter 3练习, spring boot 初始搭建 以及spring-boot-jdbc的理解与配置

--- chapter3/src/main/java/com/smart/chapter3/Chapter3Application.java
+++ chapter3/src/main/java/com/smart/chapter3/Chapter3Application.java
@@ -3,12 +3,16 @@ package com.smart.chapter3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// @SpringBootApplication //@SpringBootApplication 被 @Configuration、@EnableAutoConfiguration、@ComponentScan 注解所修饰,换言之 Springboot 提供了统一的注解来替代以上三个注解
+//@SpringBootApplication //@SpringBootApplication 被 @Configuration、@EnableAutoConfiguration、@ComponentScan 注解所修饰,换言之 Springboot 提供了统一的注解来替代以上三个注解
 @RestController
 @EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class Chapter3Application {
 
     @RequestMapping("/")
