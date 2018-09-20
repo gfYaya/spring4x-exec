dc077fd5df7b14b1ca4e6f30167778bacb108be2
Zhouyan <2645919729@qq.com>
2018-09-20 18:05:09 +0800

    spring4x 自学项目chapter 4 小问题

--- chapter4/src/main/java/com/smart/beanfactory/MyBeanPostProcessor.java
+++ chapter4/src/main/java/com/smart/beanfactory/MyBeanPostProcessor.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
  */
 public class MyBeanPostProcessor implements BeanPostProcessor{
 
-    @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         if("car".equals(beanName)){
             Car car = (Car)bean;
@@ -21,8 +20,7 @@ public class MyBeanPostProcessor implements BeanPostProcessor{
         }
         return bean;
     }
-
-    @Override
+    
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if("car".equals(beanName)){
             Car car = (Car)bean;
