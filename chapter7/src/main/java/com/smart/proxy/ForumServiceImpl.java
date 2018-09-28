d1bf7132a0229ef476eb26da4ecca9426994ac5c
Zhouyan <2645919729@qq.com>
2018-09-28 18:16:01 +0800

    spring4x 自学项目chapter 7,AOP入门和横切逻辑特征

---
+++ chapter7/src/main/java/com/smart/proxy/ForumServiceImpl.java
@@ -0,0 +1,33 @@
+package com.smart.proxy;
+
+public class ForumServiceImpl implements ForumService {
+
+	public void removeTopic(int topicId) {
+		//开始对该方法进行性能监测
+		//PerformanceMonitor.begin("com.smart.proxy.ForumServiceImpl.removeTopic");
+
+		System.out.println("模拟删除Topic记录:"+topicId);
+		try {
+			Thread.currentThread().sleep(20);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		//结束对该方法的性能监测
+		//PerformanceMonitor.end();
+	}
+
+	public void removeForum(int forumId) {
+		//PerformanceMonitor.begin("com.smart.proxy.ForumServiceImpl.removeForum");
+
+		System.out.println("模拟删除Forum记录:"+forumId);
+		try {
+			Thread.currentThread().sleep(40);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		//PerformanceMonitor.end();
+	}
+
+}
