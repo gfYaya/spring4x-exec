68aeb974e4f4828ad0d357e3e29df5637223890d
Zhouyan <2645919729@qq.com>
2018-09-28 08:53:38 +0800

    spring4x 自学项目chapter 6

--- chapter6/src/main/java/com/smart/i18n/I18nGreeting.java
+++ chapter6/src/main/java/com/smart/i18n/I18nGreeting.java
@@ -48,16 +48,24 @@ public class I18nGreeting {
 		String[] configs = {"com/smart/i18n/beans.xml"};
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(configs);
 		Object[] params = {"John", new GregorianCalendar().getTime()};
-		
+
+		/* 并未如上述的方法,去直接获取某个Bean (getBean() ) 或者直接拿到MessageResource,因为beans.xml中已经注册一个名为
+		      messageSource的bean,如果未注册,则报出 org.springframework.context.NoSuchMessageException 的异常.
+		reason: 在AbstractApplicationContext中的refresh()方法中的一步操作,initMessageSource()就是初始化容器中的国际化信息资源,它根据反射
+           机制从BeanDefinitionRegistry中找出名为messageSource且类型为org.springframework.context.MessageResource的Bean.将这个Bean定义
+            的信息资源加载为容器级的国际化信息资源.
+		*/
 		String str1 = ctx.getMessage("greeting.common",params,Locale.US);
 		String str2 = ctx.getMessage("greeting.morning",params,Locale.CHINA);	
 		System.out.println(str1);
 		System.out.println(str2);	
-	}	
-	
+	}
+
 	public static void main(String[] args) throws Exception{
-		rsrBdlMessageResource();
-		rrsrBdlMessageResource();
+//		rsrBdlMessageResource();
+//		rrsrBdlMessageResource();
 		ctxMessageResource();
 	}
+
+
 }
