ef41d332cd45686618372e36dab6cb6e149a2f0e
Zhouyan <2645919729@qq.com>
2018-09-29 18:02:33 +0800

    spring4x 自学项目chapter 7,动态代理 实现AOP 各层抽象出来,实现增强 书中P218 图7-2

--- chapter7/src/main/java/com/smart/proxy/PerformaceHandler.java
+++ chapter7/src/main/java/com/smart/proxy/PerformaceHandler.java
@@ -5,14 +5,19 @@ import java.lang.reflect.Method;
 
 public class PerformaceHandler implements InvocationHandler {
     private Object target;
-	public PerformaceHandler(Object target){
+
+	public PerformaceHandler(Object target){ //target为目标业务类
 		this.target = target;
 	}
-	public Object invoke(Object proxy, Method method, Object[] args)
-			throws Throwable {
+
+	/*  Processes a method invocation on a proxy instance and returns the result.
+	 *  @param  proxy the proxy instance that the method was invoked on
+	 */
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		PerformanceMonitor.begin(target.getClass().getName()+"."+ method.getName());
-		Object obj = method.invoke(target, args);
+		Object obj = method.invoke(target, args); //通过反射类调用目标业务类方法
 		PerformanceMonitor.end();
 		return obj;
 	}
+
 }
--- chapter7/src/test/java/com/smart/proxy/ForumServiceTest.java
+++ chapter7/src/test/java/com/smart/proxy/ForumServiceTest.java
@@ -14,19 +14,24 @@ public class ForumServiceTest {
 //		 forumService.removeTopic(1012);
 
 		// 使用JDK动态代理
-		ForumService target = new ForumServiceImpl();
-		PerformaceHandler handler = new PerformaceHandler(target);
-		ForumService proxy = (ForumService) Proxy.newProxyInstance(target
-				.getClass().getClassLoader(),
+		//ForumService target = new ForumServiceImpl(); //希望被代理的目标业务类
+		ForumServiceImpl target = new ForumServiceImpl();
+		PerformaceHandler handler = new PerformaceHandler(
+				target  //将目标业务类和横切编织到一起
+		                                                  );
+		//根据编织了目标业务类逻辑和性能监视横切逻辑的InvocationHandler实例创建代理实例
+		ForumService proxy = (ForumService) Proxy.newProxyInstance(target.getClass().getClassLoader(),
+				//JDK动态代理只能对实现了接口的类生成代理，而不能针对类. 这里面的接口指的是 target.getClass().getInterfaces()
+				// 因此即便是target不使用对象上转型也可以
 				target.getClass().getInterfaces(), handler);
 		proxy.removeForum(10);
 		proxy.removeTopic(1012);
 
 		//使用CGLib动态代理
-		CglibProxy cglibProxy = new CglibProxy();
-		ForumService forumService2 = (ForumService)cglibProxy.getProxy(ForumServiceImpl.class);
-		forumService2.removeForum(10);
-		forumService2.removeTopic(1023);
+//		CglibProxy cglibProxy = new CglibProxy();
+//		ForumService forumService2 = (ForumService)cglibProxy.getProxy(ForumServiceImpl.class);
+//		forumService2.removeForum(10);
+//		forumService2.removeTopic(1023);
 		
 	}
 	 
