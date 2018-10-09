e780e65c66cc10e2c91188a27834af3a67f1eba4
gfYaya <2645919729@qq.com>
2018-10-09 23:47:16 +0800

    spring 企业实战4.x ,chapter 7,AOP PintCut和Advise

--- chapter7/src/main/java/com/smart/advisor/GreetingDynamicPointcut.java
+++ chapter7/src/main/java/com/smart/advisor/GreetingDynamicPointcut.java
@@ -4,27 +4,32 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.aop.ClassFilter;
 import org.springframework.aop.support.DynamicMethodMatcherPointcut;
 
+// DynamicMethodMatcherPointcut是一个抽象类,他将isRuntime()标识为final且返回true,这样其子类一定是一个动态切点
 public class GreetingDynamicPointcut extends DynamicMethodMatcherPointcut {
 	private static List<String> specialClientList = new ArrayList<String>();
 	static {
 		specialClientList.add("John");
 		specialClientList.add("Tom");
 	}
-//	public ClassFilter getClassFilter() {
-//		return new ClassFilter() {
-//			public boolean matches(Class clazz) {
-//				System.out.println("调用getClassFilter()对"+clazz.getName()+"做静态检查.");
-//				return Waiter.class.isAssignableFrom(clazz);
-//			}
-//		};
-//	}
-//	public boolean matches(Method method, Class clazz) {
-//		System.out.println("调用matches(method,clazz)对"+clazz.getName()+"."+method.getName()+"做静态检查.");
-//		return "greetTo".equals(method.getName());
-//	}
-	public boolean matches(Method method, Class clazz, Object[] args) {
+
+	public ClassFilter getClassFilter() { //对类进行静态切点检查
+		return new ClassFilter() {
+			public boolean matches(Class clazz) {
+				System.out.println("调用getClassFilter()对"+clazz.getName()+"做静态检查.");
+				return Waiter.class.isAssignableFrom(clazz);
+			}
+		};
+	}
+
+	public boolean matches(Method method, Class clazz) { //对方法进行静态切点检查
+		System.out.println("调用matches(method,clazz)对"+clazz.getName()+"."+method.getName()+"做静态检查.");
+		return "greetTo".equals(method.getName());
+	}
+
+	public boolean matches(Method method, Class clazz, Object[] args) { //对方法进行动态切点检查
 		System.out.println("调用matches(method,clazz)对"+clazz.getName()+"."+method.getName()+"做动态检查.");
 		String clientName = (String) args[0];
 		return specialClientList.contains(clientName);
--- chapter7/src/main/java/com/smart/advisor/WaiterDelegate.java
+++ chapter7/src/main/java/com/smart/advisor/WaiterDelegate.java
@@ -2,7 +2,8 @@ package com.smart.advisor;
 
 public class WaiterDelegate {
 	private Waiter waiter;
-	public void service(String clientName) {
+
+	public void service(String clientName) { //waiter的方法通过该方法进行调用
 		waiter.greetTo(clientName);
 		waiter.serveTo(clientName);
 	}
