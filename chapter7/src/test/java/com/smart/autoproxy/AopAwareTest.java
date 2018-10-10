7b5db316d7e4ae4fa563d5a8bd3fc3c1a911a10e
gfYaya <2645919729@qq.com>
2018-10-11 00:02:27 +0800

    spring 企业实战4.x ,chapter 7,AOP 7.5 部分 AOP不可实现部分 始终源代码达到效果 但是自己写的代码却无效 正在排查问题

---
+++ chapter7/src/test/java/com/smart/autoproxy/AopAwareTest.java
@@ -0,0 +1,17 @@
+package com.smart.autoproxy;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.annotations.Test;
+
+public class AopAwareTest {
+	@Test
+	public void autoProxy() {
+		String configPath = "com/smart/autoproxy/beans-aware.xml";
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(configPath);
+		Waiter waiter = (Waiter) ctx.getBean("waiter");
+		waiter.serveTo("John");
+		//waiter.greetTo("John");
+	}
+
+}
