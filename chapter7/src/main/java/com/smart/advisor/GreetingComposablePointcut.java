125211b9722a07cbeb549da6b67ef0ab13de886f
Zhouyan <2645919729@qq.com>
2018-10-11 08:44:42 +0800

    spring4x 自学项目chapter 7

--- chapter7/src/main/java/com/smart/advisor/GreetingComposablePointcut.java
+++ chapter7/src/main/java/com/smart/advisor/GreetingComposablePointcut.java
@@ -6,11 +6,12 @@ import org.springframework.aop.support.ControlFlowPointcut;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 
 public class GreetingComposablePointcut {
+
    public Pointcut getIntersectionPointcut(){
-	   ComposablePointcut cp = new ComposablePointcut();
-	   Pointcut pt1 = new ControlFlowPointcut(WaiterDelegate.class,"service");
-	   NameMatchMethodPointcut pt2 = new NameMatchMethodPointcut();
+	   ComposablePointcut cp = new ComposablePointcut(); //创建一个复合切点
+	   Pointcut pt1 = new ControlFlowPointcut(WaiterDelegate.class,"service");//创建一个流程切点
+	   NameMatchMethodPointcut pt2 = new NameMatchMethodPointcut();//创建一个方法名切点
 	   pt2.addMethodName("greetTo");
-	   return cp.intersection(pt1).intersection((Pointcut)pt2);    
+	   return cp.intersection(pt1).intersection((Pointcut)pt2); //两个切点进行交集操作
    }
 }
