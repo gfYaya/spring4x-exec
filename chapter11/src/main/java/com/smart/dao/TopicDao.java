f06d4fe4f2f8c838c6f4191cc0b530419fabae28
Zhouyan <2645919729@qq.com>
2018-10-23 10:36:21 +0800

    spring4x 自学项目chapter 11 @Transactional 和事务理解

---
+++ chapter11/src/main/java/com/smart/dao/TopicDao.java
@@ -0,0 +1,37 @@
+package com.smart.dao;
+
+import com.smart.domain.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TopicDao {
+	private JdbcTemplate jdbcTemplate;
+
+	public void addTopic(final Topic topic) {
+		final String sql = "INSERT INTO t_topic(topic_title) VALUES(?)";
+        		
+//		KeyHolder keyHolder = new GeneratedKeyHolder();
+//		getJdbcTemplate().update(new PreparedStatementCreator() {
+//			public PreparedStatement createPreparedStatement(Connection conn)
+//					throws SQLException {
+//				PreparedStatement ps = conn.prepareStatement(sql);
+//				ps.setString(1, topic.getTopicTitle());
+//				return ps;
+//			}
+//		}, keyHolder);
+//		topic.setTopicId(keyHolder.getKey().intValue());
+		
+		Object[] params = new Object[]{topic.getTopicTitle()};
+		jdbcTemplate.update(sql, params);
+	}
+
+	@Autowired
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+}
